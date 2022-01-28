fec03d2374d1ce763e69d0c77ac410e13dac6108
Sean Flanigan <seanflaniganjr6588@gmail.com>
2022-01-27 19:13:14 -0600

    Included functionality and methods

--- Asteroid_Angular/Asteroid/projects/asteroid-app/src/app/home-page/home-page.component.ts
+++ Asteroid_Angular/Asteroid/projects/asteroid-app/src/app/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { NASAApiService } from '../services/nasa-api.service';
 import { Asteroids } from '../Models/Asteroids';
 import { QuotesApiService } from '../services/quotes-api.service';
 import { Quotes } from '../Models/Quotes';
+import { Quote, templateJitUrl } from '@angular/compiler';
 
 
 @Component({
@@ -14,17 +15,24 @@ import { Quotes } from '../Models/Quotes';
 export class HomePageComponent implements OnInit {
 
   asteroids: any
+  input: Number = 0
+  tempQuote: Quotes
+
   constructor(
     private httpClient: HttpClient,
     private NASAAPISvc: NASAApiService,
     private quotesAPISvc: QuotesApiService
-  ) {}
+  ) {this.tempQuote = new Quotes(0,"","");}
 
   ngOnInit(): void {
-    //this.getMiles();
-    //this.getNames();
-    //this.getHazard();
-    this.getQuotes();
+    // this.getMiles();
+    // this.getNames();
+    // this.getHazard();
+    // this.getQuotes();
+    //this.tempQuote = new Quotes(0,"","");
+    console.log(this.tempQuote)
+    this.getNumberOfAsteroids(this.calculateChance);
+    //this.calculateChance();
   }
 
 
@@ -70,9 +78,10 @@ export class HomePageComponent implements OnInit {
     })
   }
 
-  getNumberOfAsteroids(){
+  getNumberOfAsteroids(retElement: (input: Number) => void)
+   {
     this.NASAAPISvc.getAsteroids().subscribe((Asteroids) => {
-      return Asteroids.element_count
+      retElement(Asteroids.element_count)
     })
   }
 
@@ -81,5 +90,84 @@ export class HomePageComponent implements OnInit {
       console.log(Quotes)
     })
   }
+
+  calculateChance(input: Number){
+    let tempQuote = new Quotes(0,"","");
+
+    console.log(input)
+    console.log(tempQuote)
+    // this.count = this.getNumberOfAsteroids();
+    if (input <= 1 && input >= 4) {
+      tempQuote.percentage = 10; 
+    }
+    else if (input == 5) {
+      tempQuote.percentage = 20;
+    } else if (input == 6) {
+      tempQuote.percentage = 30;
+    } else if (input == 7) {
+      tempQuote.percentage = 40;
+    } else if (input == 8) {
+      tempQuote.percentage = 50;
+    } else if (input == 9) {
+      tempQuote.percentage = 60;
+    } else if (input == 10) {
+      tempQuote.percentage = 70;
+    } else if (input == 11) {
+      tempQuote.percentage = 80;
+    } else if (input == 12) {
+      tempQuote.percentage = 90;
+    } else if (input >= 13) {
+      tempQuote.percentage = 100;
+     } //else {
+    //   tempQuote.percentage = 17;
+    // }
+
+    console.log(tempQuote.percentage);
+   //   case (this.count >= 1 && this.count <= 4);
+    //   this.tempQuote.percentage = 10;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 5:
+    //   this.tempQuote.percentage = 20;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 6:
+    //   this.tempQuote.percentage = 30;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 7:
+    //   this.tempQuote.percentage = 40;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 8:
+    //   this.tempQuote.percentage = 50;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 9:
+    //   this.tempQuote.percentage = 60;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 10:
+    //   this.tempQuote.percentage = 70;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 11:
+    //   this.tempQuote.percentage = 80;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count = 12:
+    //   this.tempQuote.percentage = 90;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    //   case this.count >= 13:
+    //   this.tempQuote.percentage = 100;
+    //   //Call Method to match percentage and pull quote
+    //   break;
+    // }
+    // console.log(this.tempQuote.percentage);
+    // return;
+
+  }
+
 
 }
