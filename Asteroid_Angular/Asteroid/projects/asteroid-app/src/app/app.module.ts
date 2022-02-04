2491766317055e5e718ff6f6d2968ed8ced35d18
Sean L <splambert1020@gmail.com>
2022-02-04 14:12:33 -0500

    delete files

--- Asteroid_Angular/Asteroid/projects/asteroid-app/src/app/app.module.ts
+++ Asteroid_Angular/Asteroid/projects/asteroid-app/src/app/app.module.ts
@@ -8,20 +8,23 @@ import { HttpClientModule } from "@angular/common/http";
 import { FanFavoritesComponent } from './fan-favorites/fan-favorites.component';
 import { FanFavoritesService } from './services/fan-favorites.service';
 import { QuotesApiService } from './services/quotes-api.service';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    FanFavoritesComponent
+    FanFavoritesComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [QuotesApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
--- Asteroid_Angular/Asteroid/projects/asteroid-app/src/app/home-page/home-page.component.ts
+++ Asteroid_Angular/Asteroid/projects/asteroid-app/src/app/home-page/home-page.component.ts
@@ -24,17 +24,21 @@ export class HomePageComponent implements OnInit {
 
   percentage: Number = 0
   quote: String = ""
-  recommendation = ""
+  recommendation: String = ""
 
   dateYYYY: Number = 0
   dateMM: Number = 0
   dateDD: Number = 0
   counter: Number = 0
 
+  //finalDate: String = `${this.dateYYYY}-${this.dateMM}-${this.dateDD}`
+  finalDate: String = "2022-01-18"
   miles: Number = 0
   kilometers: Number = 0
   title: any
   amount:Number = 0
+
+  linkToHome: String = '#';
 
   constructor(
     private httpClient: HttpClient,
@@ -45,7 +49,7 @@ export class HomePageComponent implements OnInit {
   ) {this.tempQuote = new Quotes(0,"",""), this.singlequote = new Quotes(0, "", "");}
 
   ngOnInit(): void {
-   // this.getMiles();
+    //this.getMiles();
     //this.getKilometers()
     //this.getName()
     //this.getCount()
@@ -55,9 +59,9 @@ export class HomePageComponent implements OnInit {
     //this.tempQuote = new Quotes(0,"","");
     //console.log(this.tempQuote)
     // this.getNumberOfAsteroids(this.calculateChance,this.quotesAPISvc);
-    //this.calculateChance();
+    this.calculateChance();
     //this.percentageMatch();
-
+    this.linkToHome = `/home`
 
   }
 
@@ -85,37 +89,85 @@ export class HomePageComponent implements OnInit {
   //     })
   // }
 
-  getMiles(dateYYYY: Number, dateMM: Number, dateDD: Number){
-    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
-    this.ourNasaAPISvc.getMiles(finalDate).subscribe((miles) => {
+  getMiles(){
+    this.ourNasaAPISvc.getMiles(this.finalDate).subscribe((miles) => {
       this.miles = miles
       console.log(this.miles)
     })
   }
 
-  getKilometers(dateYYYY: Number, dateMM: Number, dateDD: Number){
-    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
-    this.ourNasaAPISvc.getKilometers(finalDate).subscribe((kilometers) =>{
+  getKilometers(){
+    this.ourNasaAPISvc.getKilometers(this.finalDate).subscribe((kilometers) =>{
       this.kilometers = kilometers
       console.log(this.kilometers)
     })
   }
 
-  getName(dateYYYY: Number, dateMM: Number, dateDD: Number){
-    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
-    this.ourNasaAPISvc.getName(finalDate).subscribe((title) =>{
+  getName(){
+    this.ourNasaAPISvc.getName(this.finalDate).subscribe((title) =>{
       this.title = title
       console.log(this.title)
     })
   }
 
-  getCount(dateYYYY: Number, dateMM: Number, dateDD: Number){
-    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
-    this.ourNasaAPISvc.getCount(finalDate).subscribe((amount) =>{
+  getCount(){
+    this.ourNasaAPISvc.getCount(this.finalDate).subscribe((amount) =>{
       this.amount = amount
       console.log(this.amount)
+      return this.amount
     })
   }
+
+ getQuotes(){
+     this.quotesAPISvc.getQuotes().subscribe((Quotes) => {
+      this.quotesList = Quotes
+   })
+  }
+
+  calculateChance(){
+    let tempQuote = new Quotes(0,"","");
+    this.amount = 15
+      if (this.amount <= 1 && this.amount >= 4) {
+         tempQuote.percentage = 10; 
+        }
+        else if (this.amount == 5) {
+        tempQuote.percentage = 20;
+        } else if (this.amount == 6) {
+        tempQuote.percentage = 30;
+        } else if (this.amount == 7) {
+        tempQuote.percentage = 40;
+        } else if (this.amount == 8) {
+        tempQuote.percentage = 50;
+        } else if (this.amount == 9) {
+        tempQuote.percentage = 60;
+        } else if (this.amount == 10) {
+        tempQuote.percentage = 70;
+        } else if (this.amount == 11) {
+        tempQuote.percentage = 80;
+        } else if (this.amount == 12) {
+        tempQuote.percentage = 90;
+        } else if (this.amount>= 13) {
+        tempQuote.percentage = 100;
+      }
+    
+
+    console.log(tempQuote)      
+
+
+    this.quotesAPISvc.getQuotes().subscribe((Quotes) => {
+    this.quotesList = Quotes
+    let singlequote = this.quotesList.find(q => q.percentage === tempQuote.percentage)
+    if (singlequote != null){
+      console.log(singlequote.percentage)
+      this.percentage = singlequote.percentage
+      console.log(singlequote.quote)
+      this.quote = this.singlequote.quote
+      console.log(singlequote.recommendation)
+      this.recommendation = singlequote.recommendation
+    } 
+  })
+  }
+
 
   // getNames(){
   //   this.NASAAPISvc.getAsteroids().subscribe((Asteroids) => {
@@ -189,33 +241,61 @@ export class HomePageComponent implements OnInit {
       
       
       
-      // let percentage = Quotes.filter(q => q.percentage === tempQuote.percentage).map(q => q.percentage)
-      // let quote = Quotes.filter(q => q.percentage === tempQuote.percentage).map(q => q.quote)
-      // let recommendation = Quotes.filter(q => q.percentage === tempQuote.percentage).map(q => q.recommendation)
-      // this.percentage = this.percentage
-      // this.quote = this.quote
-      // this.recommendation = this.recommendation
-      // console.log(percentage)
-      // console.log(quote)
-      // console.log(recommendation)})
+  // let percentage = Quotes.filter(q => q.percentage === tempQuote.percentage).map(q => q.percentage)
+  // let quote = Quotes.filter(q => q.percentage === tempQuote.percentage).map(q => q.quote)
+  // let recommendation = Quotes.filter(q => q.percentage === tempQuote.percentage).map(q => q.recommendation)
+  // this.percentage = this.percentage
+  // this.quote = this.quote
+  // this.recommendation = this.recommendation
+  // console.log(percentage)
+  // console.log(quote)
+  // console.log(recommendation)})
 
    
  
-      // addtofavorites(){
-      //   let favorite = new FanFavorites(id, percentage, date, counter)
-      //   this.favoritesAPISvc.createFavorite(favorite).subscribe((favorite) =>{
-      //     console.log(["STUFF"])
-      //     console.log(favorite)
-      //   })
-      // }
+  // addtofavorites(){
+  //   let favorite = new FanFavorites(id, percentage, date, counter)
+  //   this.favoritesAPISvc.createFavorite(favorite).subscribe((favorite) =>{
+  //     console.log(["STUFF"])
+  //     console.log(favorite)
+  //   })
+  // }
 
-      addToFavorites(){
-        let favorite = new FanFavorites(0, 0, "", 0, "", "")
-      }
+  getMiles(dateYYYY: Number, dateMM: Number, dateDD: Number){
+    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
+    this.ourNasaAPISvc.getMiles(finalDate).subscribe((miles) => {
+      this.miles = miles
+      console.log(this.miles)
+    })
+  }
+    
+  getKilometers(dateYYYY: Number, dateMM: Number, dateDD: Number){
+    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
+    this.ourNasaAPISvc.getKilometers(finalDate).subscribe((kilometers) =>{
+      this.kilometers = kilometers
+       console.log(this.kilometers)
+    })
+  }
+    
+  getName(dateYYYY: Number, dateMM: Number, dateDD: Number){
+    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
+    this.ourNasaAPISvc.getName(finalDate).subscribe((title) =>{
+      this.title = title
+      console.log(this.title)
+    })
+  }
+    
+  getCount(dateYYYY: Number, dateMM: Number, dateDD: Number){
+    let finalDate: String = this.getFinalDate(dateYYYY, dateMM, dateDD)
+    this.ourNasaAPISvc.getCount(finalDate).subscribe((amount) =>{
+      this.amount = amount
+      console.log(this.amount)
+    })
+  }
 
-      getFinalDate(dateYYYY: Number, dateMM: Number, dateDD: Number): String{
-        let finalDate: String = dateYYYY + "-" + dateMM + "-" + dateDD
-        return finalDate
-      }
-  //}
+  getFinalDate(dateYYYY: Number, dateMM: Number, dateDD: Number): String{
+    let finalDate:String = dateYYYY + "-" + dateMM + "-" + dateDD
+    return finalDate
+  }
+  
 }
